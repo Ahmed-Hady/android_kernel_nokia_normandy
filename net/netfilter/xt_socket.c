47919f2b2316f0187f1828ad4351401a27ac8c5d
lukino563 <lukino563@gmail.com>
2015-07-19 22:54:10 +0200

    net: fix

--- net/netfilter/xt_socket.c
+++ net/netfilter/xt_socket.c
@@ -276,7 +276,7 @@ xt_socket_get6_sk(const struct sk_buff *skb, struct xt_action_param *par)
 	struct ipv6hdr ipv6_var, *iph = ipv6_hdr(skb);
 	struct udphdr _hdr, *hp = NULL;
 	struct sock *sk;
-	const struct in6_addr *daddr = NULL, *saddr = NULL;
+	const struct in6_addr *daddr, *saddr; //const struct in6_addr *daddr = NULL, *saddr = NULL;
 	__be16 uninitialized_var(dport), uninitialized_var(sport);
 	int thoff = 0, uninitialized_var(tproto);
 
